dd0d10b782613cdbb8a190db2b9a5a146e7223c8
Kexin Zheng <kexinz0227@gmail.com>
2024-03-14 21:25:48 -0700

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -72,11 +72,10 @@ def load_quantized_model(model_name, quantization):
         os.environ["BNB_4BIT_COMPUTE_DTYPE"] = "float16"
     elif quantization == "qlora":
         bnb_config = BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_quant_type="nf4",
-        bnb_4bit_compute_dtype=torch.bfloat16,
-        bnb_4bit_use_double_quant=True
-        )
+            load_in_4bit=True,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_compute_dtype=torch.bfloat16,
+            bnb_4bit_use_double_quant=True)
         model = AutoModelForCausalLM.from_pretrained(model_name, quantization_config=bnb_config, device_map="auto")
         tokenizer = AutoTokenizer.from_pretrained(
                 model_name,
